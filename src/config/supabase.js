5077b4ffee50c46854008c580f9adcab1838bf9c
19Vin70 <alqasimquillo@gmail.com>
2024-05-05 23:17:14 +0800

    New commit

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://flfapssjsncisklbnhrt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZmFwc3Nqc25jaXNrbGJuaHJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4ODQxNzUsImV4cCI6MjAzMDQ2MDE3NX0.rZq51LkyprL0BEcn5jxUjZx-c0dMLcy09pG71feqEGw'
+const supabaseUrl = ''
+const supabaseKey = ''
 export const supabase = createClient(supabaseUrl, supabaseKey)
