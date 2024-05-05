4b8f986aa0a082425f4d001d1037933561967ad2
19Vin70 <alqasimquillo@gmail.com>
2024-05-05 23:18:31 +0800

    New commit

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = ''
-const supabaseKey = ''
+const supabaseUrl = '' // supabase project url
+const supabaseKey = '' //supabase anon key
 export const supabase = createClient(supabaseUrl, supabaseKey)
